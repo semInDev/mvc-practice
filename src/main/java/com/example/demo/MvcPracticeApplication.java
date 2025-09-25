5b10e504e2f8b3bbf37115a79baaab64c8f99ff3
sem_in_github <semin6634@naver.com>
2025-09-25 13:20:30 +0900

    init: 초기 세팅

---
+++ src/main/java/com/example/demo/MvcPracticeApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcPracticeApplication { // 여기 메인 메소드를 실행하면 애플리케이션이 실행됨!
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		// SpringApplication.run(MvcPracticeApplication.class, args);
+	}
+
+}
